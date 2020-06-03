4cf20a5b6b08373c71e708d27d7d1bfebb5fd1eb
RitaMarques <r2016700@novaims.unl.pt>
2020-06-03 16:05:46 +0100

    data1000

--- Project_CNN.py
+++ Project_CNN.py
@@ -1,3 +1,4 @@
+
 import os, random
 import string
 import shutil
@@ -11,13 +12,13 @@ import matplotlib.pyplot as plt
 # #-----------------------------------------------------------------------------------------------------------------------
 # # PREPROCESS DATA
 # #-----------------------------------------------------------------------------------------------------------------------
-# basedir = r'.data\PREPROCESS DATASET'
-# destinydir = r'.data\IMAGENS_ALL'
-#
-# # Alphabet
-# alphabet_upper = list(string.ascii_uppercase)
-# alphabet_lower = list(string.ascii_lowercase)
-#
+basedir = r'C:\Users\TITA\Downloads\PREPROCESS DATASET'
+destinydir = r'C:\Users\TITA\Downloads\data'
+
+# Alphabet
+alphabet_upper = list(string.ascii_uppercase)
+alphabet_lower = list(string.ascii_lowercase)
+
 # counts = {}
 # for letter in alphabet_lower:
 #         counts["count_{0}".format(letter)] = 0
@@ -46,7 +47,6 @@ import matplotlib.pyplot as plt
 # # ORGANIZE DATA INTO TRAIN, VALIDATION AND TEST DIRECTORIES
 # #-----------------------------------------------------------------------------------------------------------------------
 #
-#
 # # create directories for the train, val and test splits
 # train_dir = os.path.join(destinydir, 'train')
 # os.mkdir(train_dir)
@@ -64,7 +64,7 @@ import matplotlib.pyplot as plt
 #
 # # 350 images to each class in training
 # # 70 images from each person's folder
-# images_per_person = 70
+# images_per_person = 400
 #
 # for idx, folder in enumerate(os.listdir(basedir)):
 #     folder_base = os.path.join(basedir, folder)
@@ -74,7 +74,6 @@ import matplotlib.pyplot as plt
 #         folder_letter_destiny = os.path.join(train_dir, letter)                             # updated to train_dir
 #
 #         images_kept = random.sample(os.listdir(folder_letter_source), k=images_per_person)
-#         # images_kept = random.choices(os.listdir(folder_letter_source), k=70)
 #
 #         for image in images_kept:
 #             src = os.path.join(folder_letter_source, image)
@@ -83,7 +82,7 @@ import matplotlib.pyplot as plt
 #
 # # move 50 images from train to validation
 # # move 50 images from train to test
-# images_for_test = 50
+# images_for_test = 500
 #
 # for index, letter in enumerate(os.listdir(train_dir)):
 #     folder_letter_source = os.path.join(train_dir, letter)
@@ -102,12 +101,13 @@ import matplotlib.pyplot as plt
 #         dst = os.path.join(folder_letter_destiny_test, image)
 #         shutil.move(src, dst)
 
+
 #-----------------------------------------------------------------------------------------------------------------------
 # DATA PRE-PROCESSING
 #-----------------------------------------------------------------------------------------------------------------------
-train_dir = r'.\data\train'
-val_dir = r'.\data\validation'
-test_dir = r'.\data\test'
+train_dir = r'.\data1000\train'
+val_dir = r'.\data1000\validation'
+test_dir = r'.\data1000\test'
 
 # creates tensors out of the data
 train_datagen = ImageDataGenerator()
@@ -116,21 +116,21 @@ test_datagen = ImageDataGenerator()
 
 train_generator = train_datagen.flow_from_directory(
     train_dir,
-    target_size=(100, 100),  # resizes all images to 150x150
+    target_size=(100, 100),  # resizes all images
     batch_size=20,
     class_mode='categorical'
 )
 
 validation_generator = val_datagen.flow_from_directory(
     val_dir,
-    target_size=(100, 100),  # resizes all images to 150x150
+    target_size=(100, 100),  # resizes all images
     batch_size=10,
     class_mode='categorical'
 )
 
 test_generator = test_datagen.flow_from_directory(
     test_dir,
-    target_size=(100, 100),  # resizes all images to 150x150
+    target_size=(100, 100),  # resizes all images
     batch_size=10,
     class_mode='categorical'
 )
@@ -142,17 +142,17 @@ test_generator = test_datagen.flow_from_directory(
 model = models.Sequential()
 
 # feature maps extracted: 32   # filter: (3x3)  slider: 1                             (width, height, feature maps)
-model.add(layers.Conv2D(100, (3, 3), activation='relu', input_shape=(150, 150, 3)))     # (148, 148,  32)
+model.add(layers.Conv2D(100, (3, 3), activation='relu', input_shape=(100, 100, 3), padding='same'))     # (148, 148,  32)
 model.add(layers.MaxPooling2D(2, 2))                                                    # ( 74,  74,  32)
-model.add(layers.Conv2D(200, (3, 3), activation='relu'))                                 # ( 72,  72,  64)
+model.add(layers.Conv2D(200, (3, 3), activation='relu', padding='same'))                                # ( 72,  72,  64)
 model.add(layers.MaxPooling2D(2, 2))                                                    # ( 36,  36,  64)
-model.add(layers.Conv2D(400, (3, 3), activation='relu'))                                # ( 34,  34, 128)
+model.add(layers.Conv2D(400, (3, 3), activation='relu', padding='same'))                                # ( 34,  34, 128)
 model.add(layers.MaxPooling2D(2, 2))                                                    # ( 17,  17, 128)
-model.add(layers.Conv2D(400, (3, 3), activation='relu'))                                # ( 15,  15, 128)
+model.add(layers.Conv2D(400, (3, 3), activation='relu', padding='same'))                                # ( 15,  15, 128)
 model.add(layers.MaxPooling2D(2, 2))                                                    # (  7,   7, 128)
 model.add(layers.Flatten())  # vectorize to one dimensional representation              # (6272)
 #model.add(layers.Dropout(0.5))
-model.add(layers.Dense(400, activation='relu'))                                         # (512) number of neurons
+#model.add(layers.Dense(400, activation='relu'))                                         # (512) number of neurons
 model.add(layers.Dense(24, activation='softmax'))                                        # (1) number of neurons
 
 # model.summary()  # get as the shapes and number of params
