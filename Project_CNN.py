0382e5fd0c577bf1ce7eef02d5aefb5a5adb75e8
RitaMarques <r2016700@novaims.unl.pt>
2020-06-04 19:40:50 +0100

    accuracy plots, save model, predict test

--- Project_CNN.py
+++ Project_CNN.py
@@ -307,8 +307,43 @@ model.add(layers.Dense(24, activation='softmax'))
 
 model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['acc'])
 
-#-----------------------------------------------------------------------------------------------------------------------
-# FITTING THE MODEL
-#-----------------------------------------------------------------------------------------------------------------------
 history = model.fit_generator(train_generator, steps_per_epoch=100, epochs=30,
                               validation_data=validation_generator, validation_steps=50)
+
+# save the model
+model.save_weights('model1_weights.h5')
+model.save('model1_keras.h5')
+
+# apply model to the test set
+pred = model.predict(test_generator)
+# analyze outputs to see how can we obtain the accuracy in test set
+
+#-----------------------------------------------------------------------------------------------------------------------
+# ANALYZING OVERFITTING
+#-----------------------------------------------------------------------------------------------------------------------
+
+# DISPLAYING CURVES OF LOSS AND ACCURACY DURING TRAINING
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(loss) + 1)
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and Validation Accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+
+plt.figure()
+
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and Validation Loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
