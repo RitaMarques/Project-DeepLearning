d252ed05c3527c6764c92390f51185163fb5f389
m2016713 <r2016713@novaims.unl.pt>
2020-06-07 22:49:23 +0100

    images report

--- Project_CNN.py
+++ Project_CNN.py
@@ -11,6 +11,7 @@ from keras import layers
 from keras import models
 from keras.preprocessing.image import ImageDataGenerator
 import matplotlib.pyplot as plt
+import seaborn as sns
 import kaggle
 from tqdm import tqdm
 import numpy as np
@@ -215,31 +216,31 @@ img_df['area'] = img_df['height'] * img_df['width']
 img_df['aspect_ratio'] = img_df['width'] / img_df['height']
 
 # plotting the height distribution
-ax = img_df['height'].hist(bins=100)
-ax.set_title('Distribution of Images\' Heights')
-ax.set_xlabel('Height of image (in pixels)')
-ax.set_ylabel('Number of images')
+plt.hist(img_df['height'], bins=100, color='darkseagreen')
+plt.title('Distribution of Images\' Heights')
+plt.xlabel('Height of image (in pixels)')
+plt.ylabel('Number of images')
 plt.show()
 
 # plotting the width distribution
-ax = img_df['width'].hist(bins=100)
-ax.set_title('Distribution of Images\' Widths')
-ax.set_xlabel('Width of image (in pixels)')
-ax.set_ylabel('Number of images')
+plt.hist(img_df['width'], bins=100, color='darkseagreen')
+plt.title('Distribution of Images\' Widths')
+plt.xlabel('Width of image (in pixels)')
+plt.ylabel('Number of images')
 plt.show()
 
 # plotting the width distribution
-ax = img_df['area'].hist(bins=100)
-ax.set_title('Distribution of Images\' Areas')
-ax.set_xlabel('Area of image (in pixels)')
-ax.set_ylabel('Number of images')
+plt.hist(img_df['area'], bins=100, color='darkseagreen')
+plt.title('Distribution of Images\' Areas')
+plt.xlabel('Area of image (in pixels)')
+plt.ylabel('Number of images')
 plt.show()
 
 # plotting the aspect ration distribution
-ax = img_df['aspect_ratio'].hist(bins=100)
-ax.set_title('Distribution of Images\' Aspect Ratios')
-ax.set_xlabel('Aspect ratio of image')
-ax.set_ylabel('Number of images')
+plt.hist(img_df['aspect_ratio'], bins=100, color='darkseagreen')
+plt.title('Distribution of Images\' Aspect Ratios')
+plt.xlabel('Aspect ratio of image (in pixels)')
+plt.ylabel('Number of images')
 plt.show()
 
 # distribution of aspect ratio by handshape
@@ -254,11 +255,18 @@ size_desc = img_df.describe()
 # joint distribution of height, width
 img_df["width_height"] = img_df[["width" , "height"]].apply(lambda row: "_".join(row.values.astype(str)) , axis=1)
 img_df["counts_w_l"] = img_df["width_height"].map(img_df["width_height"].value_counts())
-x= img_df["width"]
-y= img_df["height"]
-z= img_df["counts_w_l"]
-plt.scatter(x, y, s=z*10, alpha=0.5, )
+plt.scatter(x=img_df["width"], y=img_df["height"], s=img_df["counts_w_l"]*10,  alpha=0.5, c='darkseagreen')
+plt.title('Joint Distribution of Height and Width')
+plt.xlabel('Width of image (in pixels)')
+plt.ylabel('Heigth of image (in pixels)')
 # TODO: plot in seaborn with hue by letter
+plt.show()
+
+# joint distribution of height, width with hue by letter
+sns.scatterplot(x=img_df["width"], y=img_df["height"], hue=img_df["handshape"], palette='muted')
+plt.title('Joint Distribution of Height and Width by Letter')
+plt.xlabel('Width of image (in pixels)')
+plt.ylabel('Heigth of image (in pixels)')
 plt.show()
 
 #-----------------------------------------------------------------------------------------------------------------------
